3621bcf88fc6ef8dde9127edf0a2dcb6c45cdf0b
Jordi Gámez <jordigamezllorach@gmail.com>
2019-07-09 18:08:06 +0200

    Initial Commit

---
+++ CreditScore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CreditScore
+//
+//  Created by Jordi Gámez on 09/07/2019.
+//  Copyright © 2019 JordiGamez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
